52a4abb47363684f43eb16658b308aa45f92ba6e
quellus <quellus7@gmail.com>
2025-04-10 17:58:40 -0600

    Cards are now fully generated from JSON

--- js/card-generator.js
+++ js/card-generator.js
@@ -1,11 +1,29 @@
 const cardContainer = document.getElementById("card-container")
 
+generateCards()
+
 function generateCards() {
     fetch('content.json')
     .then(response => response.json())
     .then(data => {
-        Object.keys(data).forEach(key => {
-            
+        cardContainer.innerHTML = ""
+        data.forEach(item => {
+            let card = `
+            <div class="col-md-6 col-lg-4 mb-5">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>${item["title"]}</h3>
+                    </div>
+                    <div class="card-body">
+                        <p>${item["short_description"]}</p>
+                        <a type="button" onclick="generateModal('${[item["id"]]}')">
+                            <img src="${[item["images"][0]]}" class="img-fluid"></img>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            `
+            cardContainer.innerHTML += card
         })
     })
 }
