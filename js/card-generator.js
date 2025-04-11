3cebfc1cb682fe0908517455b39dbe8693020c9b
quellus <quellus7@gmail.com>
2025-04-10 18:32:40 -0600

    Fixed card images not centering properly

--- js/card-generator.js
+++ js/card-generator.js
@@ -10,17 +10,17 @@ function generateCards() {
         data.forEach(item => {
             let card = `
             <div class="col-md-6 col-lg-4 mb-5">
-                <div class="card">
-                    <div class="card-header">
-                        <h3>${item["title"]}</h3>
+                <a type="button" onclick="generateModal('${[item["id"]]}')">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3>${item["title"]}</h3>
+                        </div>
+                        <div class="card-body">
+                            <p>${item["short_description"]}</p>
+                            <img src="${[item["images"][0]]}" class="card-img-bottom"></img>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <p>${item["short_description"]}</p>
-                        <a type="button" onclick="generateModal('${[item["id"]]}')">
-                            <img src="${[item["images"][0]]}" class="img-fluid"></img>
-                        </a>
-                    </div>
-                </div>
+                </a>
             </div>
             `
             cardContainer.innerHTML += card
